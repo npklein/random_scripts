484e7c4e8fa94f576192eac4df627828ab82edec
npklein <niekdeklein@gamail.com>
2017-08-04 10:21:39 +0200

    write a and bCounts out

--- genetics/ASE/create_geneAE_matrix.py
+++ genetics/ASE/create_geneAE_matrix.py
@@ -4,7 +4,7 @@ import os
 
 parser = argparse.ArgumentParser(description='Make matrix (genes x samples) of the gene counts and total depth for all results in given dir + subdirs. IMPORTANT: sample names are taken by splitting bam name on . and taking first [0] element.')
 parser.add_argument('geneAE_rootdir', help='Rootdir where all gene counts can be found. Will walk through subdirs and use all *.txt files')
-parser.add_argument('out_prefix', help='Write output to <out_prefix>.geneCounts.txt and <out_prefix>.totalDepth.txt')
+parser.add_argument('out_prefix', help='Write output to <out_prefix>.geneCounts.txt, <out_prefix>.totalDepth.txt, and <out_prefix>.alleleCounts.txt')
 parser.add_argument('--verbose', action='store_true',help='If set, print out which files are being used')
 
 args = parser.parse_args()
@@ -37,23 +37,33 @@ for subdir, dirs, files in os.walk(args.geneAE_rootdir):
                 gene = line[3]
                 log2_aFC = line[7]
                 totalCount = line[6]
+                aCount = line[4]
+                bCount = line[5]
                 if not gene in genes:
                     genes.append(gene)
-
-                gene_data[name][gene] = {'log2_aFC':log2_aFC, 'totalCount':totalCount}
+                gene_data[name][gene] = {'log2_aFC':log2_aFC, 'totalCount':totalCount,
+                                         'aCount':aCount,'bCount':bCount}
 
 print('writing data')
 with open(args.out_prefix+'.geneCounts.txt','w') as outLog2_aFC, open(args.out_prefix+'.totalDepth.txt','w') as outTotalCount:
-    for name in sample_names:
-        outLog2_aFC.write('\t'+name)
-        outTotalCount.write('\t'+name)
-    outLog2_aFC.write('\n')
-    outTotalCount.write('\n')
-    for gene in genes:
-        outLog2_aFC.write(gene)
-        outTotalCount.write(gene)
+    with open(args.out_prefix+'.alleleCounts.txt','w') as outAlleleCounts:
         for name in sample_names:
-            outLog2_aFC.write('\t'+str(gene_data[name][gene]['log2_aFC']))
-            outTotalCount.write('\t'+str(gene_data[name][gene]['totalCount']))
+            outLog2_aFC.write('\t'+name)
+            outTotalCount.write('\t'+name)
+            outAlleleCounts.write('\t'+name+'_aCount')
+            outAlleleCounts.write('\t'+name+'_bCount')
         outLog2_aFC.write('\n')
         outTotalCount.write('\n')
+        outAlleleCounts.write('\n')
+        for gene in genes:
+            outLog2_aFC.write(gene)
+            outTotalCount.write(gene)
+            outAlleleCounts.write(gene)
+            for name in sample_names:
+                outLog2_aFC.write('\t'+str(gene_data[name][gene]['log2_aFC']))
+                outTotalCount.write('\t'+str(gene_data[name][gene]['totalCount']))
+                outAlleleCounts.write('\t'+str(gene_data[name][gene]['aCount']))
+                outAlleleCounts.write('\t'+str(gene_data[name][gene]['bCount']))
+            outLog2_aFC.write('\n')
+            outTotalCount.write('\n')
+            outAlleleCounts.write('\n')
